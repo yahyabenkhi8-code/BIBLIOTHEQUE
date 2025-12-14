461fad89e8f03cb3c82e7afc2c7e7252b2f441fc
yahya <yahyabenkhi8@gmail.com>
2025-12-14 23:13:55 +0100

    i creat connection with database

---
+++ config/database.php
@@ -0,0 +1,16 @@
+<?php
+$host = "localhost";
+$dbname = "bibliotheque_db";
+$user = "root";
+$pass = "";
+
+try {
+    $pdo = new PDO(
+        "mysql:host=$host;dbname=$dbname;charset=utf8",
+        $user,
+        $pass,
+        [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]
+    );
+} catch (PDOException $e) {
+    die("Erreur connexion : " . $e->getMessage());
+}
